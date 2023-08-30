d21acfabeea9d210aefbf19321aa6fc27543e83a
Williams <akanniwilliams@gmail.com>
2023-08-30 20:44:47 +0100

    'Implement a fuction to find the Lowest common ancestor of two nodes in a BT'

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *firstTemp = NULL, *secondTemp = NULL;
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first->parent == second)
+		return (first->parent);
+	if (second->parent == first)
+		return (second->parent);
+
+	firstTemp = first->parent;
+	secondTemp = second->parent;
+	while(firstTemp != NULL)
+	{
+		while(secondTemp != NULL)
+		{
+			if(firstTemp == secondTemp)
+				return (firstTemp);
+			secondTemp = secondTemp->parent;
+		}
+			firstTemp = firstTemp->parent;
+	}
+	return (NULL);
+}
